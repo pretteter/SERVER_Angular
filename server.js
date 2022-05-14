4cd7a6bb5913667f8fbce492d0186ca8c7db1b25
pretteter <52958596+pretteter@users.noreply.github.com>
2022-05-14 21:00:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,28 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
-const uri =
-  "mongodb://mongodb+srv://Pretteter:qQoSiZGnUSqThw6F@cluster0.mqyzw.mongodb.net/ToDO?retryWrites=true&w=majority";
+// const uri =
+//   "mongodb://mongodb+srv://Pretteter:qQoSiZGnUSqThw6F@cluster0.mqyzw.mongodb.net/ToDO?retryWrites=true&w=majority";
 
 // Connect to MongoDB databasee
+// returnUri();
+function returnUri() {
+  const username = encodeURIComponent("Pretteter");
+  const password = encodeURIComponent("uQOYl1gGRFDogFgA");
+  const cluster = "cluster0";
+  // const authSource = "<authSource>";
+  // const authMechanism = "<authMechanism>";
+  let uri1 = `mongodb+srv://${username}:${password}@${cluster}.mqyzw.mongodb.net/ToDO?retryWrites=true&w=majority`;
+  // console.log(uri1);
+  return uri1;
+}
+
+const options = {
+  useNewUrlParser: true,
+  // family: 4, // Use IPv4, skip trying IPv6
+};
 mongoose
-  .connect(uri)
+  .connect(returnUri(), options)
   .then(() => {
     var cors = require("cors");
     const app = express();
